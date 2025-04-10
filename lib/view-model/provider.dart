3482baf37cd1ff2baa4f4ec62dbfb03e9a8bb487
HaiderAli228 <flutter2830@gmail.com>
2025-04-10 09:50:05 +0500

    applying the provider state mang

---
+++ lib/view-model/provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:todo/model/database.dart';
+import 'package:todo/model/todo_model.dart';
+
+class NotesProvider extends ChangeNotifier {
+  List<TodoModel> _todos = [];
+  List<TodoModel> get todo => _todos;
+
+  Future<void> loadTodo() async {
+    _todos = await DatabaseHelper.instance.fetchAddTodo();
+    notifyListeners();
+  }
+
+  Future<bool> addNotes(
+    String title,
+    String priority,
+    int? reminder,
+  ) async {
+    bool success = await DatabaseHelper.instance
+        .addTodo(titleIs: title, reminder: reminder, priorityIs: priority);
+    if (success) await loadTodo();
+    return success;
+  }
+}
